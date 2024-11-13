d44b143a99016f61cfa3d9266888f373c4533a64
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-11-13 14:59:49 +0530

    imported the 3D model

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -3,11 +3,11 @@ import { Canvas } from '@react-three/fiber';
 
 // Custom imports 
 import CanvasLoader from '../components/CanvasLoader';
+import DemoComputer from '../components/DemoComputer';
 
 // Data import 
 import { myProjects } from '../constants'
-import { Center } from '@react-three/drei';
-import DemoComputer from '../components/DemoComputer';
+import { Center, OrbitControls } from '@react-three/drei';
 
 
 const Projects = () => {
@@ -97,7 +97,7 @@ const Projects = () => {
 			{/* Project Right side container - 3D Demo */}
 			<div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
 				<Canvas>
-					<ambientLight intensity={1}/>
+					<ambientLight intensity={Math.PI}/>
 					<directionalLight position={[10, 10, 5]} intensity={1}/>
 
 					<Center>
@@ -107,6 +107,12 @@ const Projects = () => {
 							</group>
 						</Suspense>
 					</Center>
+
+					{/* Orbit controls */}
+					<OrbitControls 
+						maxPolarAngle={Math.PI/2}
+						enableZoom={false}
+					/>
 				</Canvas>
 			</div>
 		</section>
