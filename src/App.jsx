3f7f46021fa08ae353a68393df12dd55162290bb
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-11-13 14:29:43 +0530

    made the project left side display

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Navbar from './sections/Navbar';
 import Hero from './sections/Hero';
 import About from './sections/About';
+import Projects from './sections/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 		<Navbar />
 		<Hero />
 		<About />
+		<Projects />
 	</main>
   )
 }
