7c957e937f0fa2db51131113181726c0f501645c
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-11-14 12:49:42 +0530

    completed the testimonials comp

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './sections/Navbar';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Projects from './sections/Projects';
+import Clients from './sections/Clients';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 		<Hero />
 		<About />
 		<Projects />
+		<Clients />
 	</main>
   )
 }
